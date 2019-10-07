9f9639bff4e97cbba4f330ef9fbc5b04f0457109
Kim Jae Hyune <wogus4041@naver.com>
2019-10-07 22:16:21 +0900

    deploy to gh-pages

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS({
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://geoje1dong.github.io/weekly_ui' : ''
-});
-
--- server.js
+++
@@ -1,4 +0,0 @@
-const app = next({
-  dev,
-  conf: {distDir: `${path.relative(process.cwd(), __dirname)}/../next`},
-});
